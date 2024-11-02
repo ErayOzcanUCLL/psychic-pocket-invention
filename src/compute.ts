369b44dbe805919e91fd9714510d95a5e7ede782
ErayOzcanUCLL <eray.ozcan@student.ucll.be>
2024-11-02 20:27:18 +0100

    server game validation done

--- src/compute.ts
+++ src/compute.ts
@@ -2,4 +2,5 @@ import { Game } from "./types";
 
 export function compute(game: Game): number {
   throw new Error("Not yet implemented"); // TODO
+  //todo also check max score 300
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import http from "http";
 import express from "express";
-import { compute } from "./compute";
+import {compute} from "./compute";
 import {Game} from "./types";
 
 export const app = express();
@@ -8,25 +8,34 @@ export const app = express();
 app.use(express.json());
 
 app.post("/compute", (request, response) => {
-  const game = request.body.game;
-  // TODO: Validate input
+    const game = request.body.game;
+    // TODO: Validate input
 // function that checks if 
-  function isValidGame(game: any): game is Game {
-    //check if its an array and has 10 frames
-    if (!Array.isArray(game) || game.length !== 10) return false;
-    
-    //check if its the frame is an array and has 2 rolls (only first 9 rolls) and if the value of the roll (pin) is btwn 0 and 10 and a number :)
-    for (let i = 0 ; i < 9; i++) {
-      const frame = game[i];
-      if (!Array.isArray(frame) || frame.length !== 2 || !frame.every(pin => typeof pin === "number" && pin >= 0 && pin <= 10)) return false;
-      
-      
-  }
-  
+    function isValidGame(game: any): game is Game {
+        //check if its an array and has 10 frames
+        if (!Array.isArray(game) || game.length !== 10) return false;
+
+        //check if its the frame is an array and has 2 rolls (only first 9 rolls) and if the value of the roll (pin) is btwn 0 and 10 and a number :)
+        for (let i = 0; i < 9; i++) {
+            const frame = game[i];
+            if (!Array.isArray(frame) || frame.length !== 2 || !frame.every(pin => typeof pin === "number" && pin >= 0 && pin <= 10)) return false;
+        }
+
+        //frame 10 heeft 3 rolls so seperate check
+        const lastFrame = game[9];
+        return Array.isArray(lastFrame) && lastFrame.length === 3 && lastFrame.every(pin => typeof pin === "number" && pin >= 0 && pin <= 10);
+    }
+
+    //validation check 
+    if (!isValidGame(game)) {
+        return response.status(400).json({error: "Invalid Game format"});
+    }
+
+    //calculate score if it's valid.
+    const score = compute(game);
 
 
-  const score = compute(game);
-  return response.json({ score });
+    return response.json({score});
 
-  // TODO: Return response
+    // TODO: Return response
 });
