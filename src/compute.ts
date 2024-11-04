dac5ca5ccac2f1792cbc205850535815ad3c2af3
ErayOzcanUCLL <eray.ozcan@student.ucll.be>
2024-11-04 23:19:07 +0100

    framepoints, and prepared empty funtions

--- src/compute.ts
+++ src/compute.ts
@@ -1,6 +1,23 @@
-import { Game } from "./types";
+import {Frame, Game, LastFrame} from "./types";
 
 export function compute(game: Game): number {
-  throw new Error("Not yet implemented"); // TODO
-  //todo also check max score 300
+    throw new Error("Not yet implemented"); // TODO
+    //todo also check max score 300
+}
+
+function framepoints(frame: Frame | LastFrame): number {
+    if (isStrike(frame) || isSpare(frame)) return 0;// will handle in other function {
+    return frameSum(frame);
+}
+
+function isStrike(frame: Frame | LastFrame): boolean {
+    throw new Error("Not yet implemented"); // TODO
+}
+
+function isSpare(frame: Frame | LastFrame): boolean {
+    throw new Error("Not yet implemented"); // TODO
+}
+
+function frameSum(frame: Frame | LastFrame): number {
+    throw new Error("Not yet implemented"); // TODO
 }
